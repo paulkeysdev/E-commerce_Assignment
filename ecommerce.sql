abafc33460b6130aa1e2d6e009ef33a7b4646c62
DawnKelvin <kelvinkibetkoech11@gmail.com>
2025-04-25 20:16:31 +0300

    All tables created

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE ecommercedb;
 
-use ecommercedb;
+USE ecommercedb;
 
 -- products table
 CREATE TABLE  products(	
@@ -8,14 +8,14 @@ product_id INT AUTO_INCREMENT PRIMARY KEY,
 category_id INT NOT NULL,
 brand_id INT NOT NULL,
 base_price DECIMAL(10, 2) NOT NULL,
-FOREIGN KEY (category_id) REFERENCES product_categories(category_id),
-FOREIGN KEY (brand_id) REFERENCES brands(brand_id),FOREIGN KEY (color_id) REFERENCES colors(color_id)
+FOREIGN KEY (category_id) REFERENCES product_category(id),
+FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
 );
 
 
 -- product image table
 CREATE TABLE product_image(
-id INT  PRIMARY KEY AUTO INCREMENT,
+id INT  PRIMARY KEY AUTO_INCREMENT,
 url VARCHAR(100),
 product_id INT NOT NULL,
 FOREIGN KEY (product_id) REFERENCES products(product_id)
@@ -31,27 +31,26 @@ name VARCHAR(100) NOT NULL
 CREATE TABLE product_item (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
 -- product_attribute table
 CREATE TABLE product_attribute (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     category_id INT,
-    color VARCHAR(200),
-    FOREIGN KEY (category_id) 
-		REFERENCES attribute_category(id),
-	FOREIGN KEY (color) REFERENCES color(name)
+    color INT,
+    FOREIGN KEY (category_id) REFERENCES attribute_category(attribute_category_id),
+	FOREIGN KEY (color) REFERENCES color(color_id)
 );
 
 -- brand table
-CREATE TABLE brand (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE brands (
+	brand_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(200)
 );
 
 
---Attribute_category table
+-- Attribute_category table
 CREATE TABLE attribute_category (
   attribute_category_id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL
@@ -60,11 +59,35 @@ CREATE TABLE attribute_category (
 -- color table
 CREATE TABLE color (
   color_id INT AUTO_INCREMENT PRIMARY KEY,
-  color_name VARCHAR(50) NOT NULL,
+  color_name VARCHAR(50) NOT NULL
 );
 
 -- Size_Category table
 CREATE TABLE size_category (
   size_category_id INT AUTO_INCREMENT PRIMARY KEY,
   category_name VARCHAR(100) NOT NULL
+);
+
+-- Create size option table
+CREATE TABLE Size_option(
+SizeOptID INT PRIMARY KEY AUTO_INCREMENT,
+Size VARCHAR (50)
+);
+
+-- Create Product_Variation table
+CREATE TABLE Product_Variation(
+ProductVarID INT PRIMARY KEY AUTO_INCREMENT,
+Product_Id INT,
+Color_Id INT,
+size_id INT,
+FOREIGN KEY (Product_Id) REFERENCES products(product_id),
+FOREIGN KEY (Color_Id) REFERENCES color(color_id),
+FOREIGN KEY (Size_Id) REFERENCES Size_option(SizeOptID)
+);
+
+-- Create attribute_type table
+
+CREATE TABLE Attribute_type(
+AttributeTypeID INT PRIMARY KEY AUTO_INCREMENT,
+AttributeType VARCHAR(50)
 );
