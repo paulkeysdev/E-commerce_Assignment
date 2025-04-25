27fd302b46ac27592602caffa3e7abe441d10406
Ademicope <michaeladekunle00@gmail.com>
2025-04-25 15:39:15 +0100

    restored deleted tables

--- ecommerce.sql
+++ ecommerce.sql
@@ -27,3 +27,44 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(100) NOT NULL
 );
 
+-- product_item table
+CREATE TABLE product_item (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+-- product_attribute table
+CREATE TABLE product_attribute (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT,
+    color VARCHAR(200),
+    FOREIGN KEY (category_id) 
+		REFERENCES attribute_category(id),
+	FOREIGN KEY (color) REFERENCES color(name)
+);
+
+-- brand table
+CREATE TABLE brand (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200)
+);
+
+
+--Attribute_category table
+CREATE TABLE attribute_category (
+  attribute_category_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+-- color table
+CREATE TABLE color (
+  color_id INT AUTO_INCREMENT PRIMARY KEY,
+  color_name VARCHAR(50) NOT NULL,
+);
+
+-- Size_Category table
+CREATE TABLE size_category (
+  size_category_id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(100) NOT NULL
+);
